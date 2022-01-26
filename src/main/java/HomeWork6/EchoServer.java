c819b01d2c889252953c1f5ecd0c6e0edfda8584
YSTerekhina <loneliness2004@mail.ru>
2022-01-26 18:17:46 +0300

    Homework done with one mistake

---
+++ src/main/java/HomeWork6/EchoServer.java
@@ -0,0 +1,61 @@
+package HomeWork6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+    Scanner scanner = new Scanner(System.in);
+    public DataOutputStream out;
+
+
+    public static void main(String[] args) {
+        new EchoServer();
+    }
+
+    public EchoServer() {
+        start();
+
+    }
+
+    private void start() {
+        Socket socket = null;
+
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился... ");
+
+            final DataInputStream in = new DataInputStream(socket.getInputStream());
+            final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            String message = in.readUTF();
+            System.out.println("Получено сообщение от клиента: " + message);
+
+            while (true) {
+                if ("/end".equalsIgnoreCase(message)) {
+                    System.out.println("Клиент отключился");
+                    out.writeUTF("/end");
+                    break;
+                }
+
+                out.writeUTF(scanner.nextLine());
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
